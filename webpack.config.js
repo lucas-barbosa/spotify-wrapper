831fbe98d3529641e5601049be6027365edf47b4
Lucas Barbosa <barbosa.lucash@gmail.com>
2019-06-29 18:32:45 -0300

    feat: add npm build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 module.exports = {
+  output: {
+    filename: 'spotifyWrapper.umd.js',
+    library: 'spotifyWrapper',
+    libraryTarget: 'umd',
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
-}
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
