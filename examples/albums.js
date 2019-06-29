057b22461d7172c0f0abc8227eeda5115ffe54dd
Lucas Barbosa <barbosa.lucash@gmail.com>
2019-06-29 18:10:13 -0300

    fix: change example for work with wrapper class

--- examples/albums.js
+++ examples/albums.js
@@ -1,5 +1,7 @@
 import SpotifyWrapper from '../src';
 
+global.fetch = require('node-fetch');
+
 const spotify = new SpotifyWrapper({
   token:
     'BQDeA7pm8nDXxxgccH950nefmLIJassxOeiptqRnOqbgTub6JDr3coiQZBPZ64tgxOtof2Zf0GU_wIzmqHwVaCI3azVxuCkRcltdHNolJ5jQmk2EAPhVtq6lJom1515-KxlGUkgoCJhxUzr7Ih-mh-adDLTdATtLAMymXhaaKT_lEHQoK7jXTegFcnmW5ylcCbF8GVLL-3GjPuy0LTno_2dvSfq_eAhWV3YFPhTSeQUpCBZ7S_HK-wsUKxLYRpCQCaoRuGTXhb_gSHKUaxlMrC-5RjtIGH_bCHQ',
