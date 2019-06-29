fe49e6e49f41b10b36c412b9f2de1aa22f9ae305
Lucas Barbosa <barbosa.lucash@gmail.com>
2019-06-29 18:09:43 -0300

    fix: change example for work with wrapper class

--- examples/albums.js
+++ examples/albums.js
@@ -1,6 +1,9 @@
-import { searchAlbums } from '../src/main';
+import SpotifyWrapper from '../src';
 
-global.fetch = require('node-fetch');
+const spotify = new SpotifyWrapper({
+  token:
+    'BQDeA7pm8nDXxxgccH950nefmLIJassxOeiptqRnOqbgTub6JDr3coiQZBPZ64tgxOtof2Zf0GU_wIzmqHwVaCI3azVxuCkRcltdHNolJ5jQmk2EAPhVtq6lJom1515-KxlGUkgoCJhxUzr7Ih-mh-adDLTdATtLAMymXhaaKT_lEHQoK7jXTegFcnmW5ylcCbF8GVLL-3GjPuy0LTno_2dvSfq_eAhWV3YFPhTSeQUpCBZ7S_HK-wsUKxLYRpCQCaoRuGTXhb_gSHKUaxlMrC-5RjtIGH_bCHQ',
+});
 
-const albums = searchAlbums('Gabriela Rocha');
+const albums = spotify.search.albums('Gabriela Rocha');
 albums.then(data => data.albums.items.map(album => console.log(album.name)));
