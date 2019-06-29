ee261c20508e4db4794e6b82458558294f76f354
Lucas Barbosa <barbosa.lucash@gmail.com>
2019-06-29 15:57:11 -0300

    refactor: code refactor on main.js

--- src/main.js
+++ src/main.js
@@ -1,26 +1,20 @@
-const api = 'https://api.spotify.com/v1';
-
-function search({ query, type }) {
-  const url = `${api}/search?q=${encodeURI(query)}&type=${encodeURI(type)}`;
+export function search({ query, type }) {
+  const url = `https://api.spotify.com/v1/search?q=${encodeURI(query)}&type=${encodeURI(type)}`;
   return fetch(url).then(data => data.json());
 }
 
-function searchAlbums(query) {
+export function searchAlbums(query) {
   return search({ query, type: 'albums' });
 }
 
-function searchArtists(query) {
+export function searchArtists(query) {
   return search({ query, type: 'artist' });
 }
 
-function searchTracks(query) {
+export function searchTracks(query) {
   return search({ query, type: 'track' });
 }
 
-function searchPlaylists(query) {
+export function searchPlaylists(query) {
   return search({ query, type: 'playlist' });
 }
-
-export {
-  search, searchAlbums, searchArtists, searchTracks, searchPlaylists,
-};
